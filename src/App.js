7ae4d51c1ce9a0a2b64cd2ca79f06e6cfaa5786c
sserges <agounkeserge@gmail.com>
2019-03-13 09:57:08 +0000

    firebase settings added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import firebase from "firebase";
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBp6VcgU82Pg9jdctPnp5hYDd5CJxhg-W4",
+      authDomain: "crm-linkedin-a8c64.firebaseapp.com",
+      databaseURL: "https://crm-linkedin-a8c64.firebaseio.com",
+      projectId: "crm-linkedin-a8c64",
+      storageBucket: "crm-linkedin-a8c64.appspot.com",
+      messagingSenderId: "909975699691"
+    });
+  }
   render() {
     return (
       <div className="App">
